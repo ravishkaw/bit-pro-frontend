bf850644c07c00b6bc289b957c72dba33209b6df
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2025-02-03 11:18:38 +0530

    bug fixes

---
+++ src/components/DataDisplay/Cards/ProfileCard.jsx
@@ -0,0 +1,74 @@
+import { Card, Col, Row } from "antd";
+import { DeleteOutlined, EditOutlined, EyeOutlined } from "@ant-design/icons";
+
+const ProfileCard = ({
+  personType,
+  columns,
+  data,
+  handleView,
+  handleEdit,
+  openDeleteModal,
+  restorePerson,
+}) => {
+  // Filter out the columns
+  const newColumns = columns.filter((column) => {
+    if (column.title === "Actions") return false; // Always remove the "Actions" column
+    if (personType !== "employee") {
+      // Remove employee-specific columns
+      if (
+        column.dataIndex === "designation" ||
+        column.dataIndex === "employeeStatus" ||
+        column.dataIndex === "empNo"
+      ) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+  return (
+    <Card
+      actions={[
+        <EyeOutlined
+          style={{ color: "blue" }}
+          onClick={() => {
+            handleView(data.id);
+          }}
+        />,
+        <EditOutlined
+          style={{ color: "#fadb14" }}
+          onClick={() => {
+            handleEdit(data.id);
+          }}
+        />,
+        <DeleteOutlined
+          style={{ color: "red" }}
+          onClick={() => openDeleteModal(data)}
+        />,
+      ]}
+      style={{ marginBottom: 10 }}>
+      {newColumns.map((column, index) => {
+        let value = data[column.dataIndex];
+        if (
+          column.dataIndex === "designation" ||
+          column.dataIndex === "employeeStatus"
+        ) {
+          value = data[column.dataIndex].name;
+        }
+
+        return (
+          <div key={index}>
+            <Row>
+              <Col span={10}>
+                <strong>{column.title}:</strong>
+              </Col>
+              <Col>{value || "N/A"}</Col>
+            </Row>
+          </div>
+        );
+      })}
+    </Card>
+  );
+};
+
+export default ProfileCard;
---
+++ src/components/DataDisplay/Table/ColumnItems.jsx
@@ -0,0 +1,98 @@
+import { Button, Space, Popconfirm } from "antd";
+import {
+  DeleteOutlined,
+  EditOutlined,
+  EyeOutlined,
+  UndoOutlined,
+} from "@ant-design/icons";
+
+export const employeeColumnItems = (
+  handleView,
+  handleEdit,
+  openDeleteModal,
+  restoreAnEmployee
+) => [
+  {
+    title: "Employee No",
+    dataIndex: "empNo",
+    // fixed: "left",
+  },
+  {
+    title: "Full Name",
+    dataIndex: "fullName",
+    fixed: "left",
+  },
+  {
+    title: "Calling Name",
+    dataIndex: "callingName",
+  },
+  {
+    title: "Phone",
+    dataIndex: "mobileNo",
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+  },
+  {
+    title: "Designation",
+    dataIndex: "designation",
+    render: (_, record) => record.designation?.name || "N/A",
+  },
+  {
+    title: "Status",
+    dataIndex: "employeeStatus",
+    render: (_, record) => record.employeeStatus?.name || "N/A",
+  },
+  {
+    title: "Actions",
+    key: "operation",
+    fixed: "right",
+    render: (_, record) => (
+      <Space size="small">
+        <Button
+          size="small"
+          color="blue"
+          variant="outlined"
+          onClick={() => {
+            handleView(record.id);
+          }}
+        >
+          <EyeOutlined />
+        </Button>
+        <Button
+          size="small"
+          color="yellow"
+          variant="outlined"
+          onClick={() => {
+            handleEdit(record.id);
+          }}
+        >
+          <EditOutlined />
+        </Button>
+        {record.employeeStatus.name !== "Deleted" ? (
+          <Button
+            size="small"
+            variant="outlined"
+            danger
+            onClick={() => openDeleteModal(record)}
+          >
+            <DeleteOutlined />
+          </Button>
+        ) : (
+          <Popconfirm
+            title="Restore the Employee"
+            description="Are you sure to restore?"
+            onConfirm={() => restoreAnEmployee(record.id)}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Button size="small" variant="outlined">
+              <UndoOutlined />
+            </Button>
+          </Popconfirm>
+        )}
+      </Space>
+    ),
+  },
+];
